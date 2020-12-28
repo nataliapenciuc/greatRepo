fde891b6fa2d0e22687d446238900a2c3dac9679
natalia <natalia.smintanca@gmail.com>
2020-12-28 15:31:43 +0200

    Initial Commit

---
+++ src/CommandAPI/Data/ICommandAPIRepo.cs
@@ -0,0 +1,18 @@
+﻿using CommandAPI.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CommandAPI.Data
+{
+    public interface ICommandAPIRepo
+    {
+        bool SaveChanges();
+        IEnumerable<Command> GetAllCommands();
+        Command GetCommandById(int id);
+        void CreateCommand(Command cmd);
+        void UpdateCommand(Command cmd);
+        void DeleteCommand(Command cmd);
+    }
+}
